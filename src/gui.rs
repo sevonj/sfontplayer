081899c813b2d566aba885aea164cff94f0b71c2
sevonj <100710152+sevonj@users.noreply.github.com>
2024-09-22 15:52:27 +0300

    Fix scrolling

--- src/gui.rs
+++ src/gui.rs
@@ -18,18 +18,7 @@ pub(crate) fn draw_gui(ctx: &egui::Context, app: &mut SfontPlayer) {
     egui::TopBottomPanel::top("sf_table")
         .resizable(true)
         .show(ctx, |ui| {
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                soundfont_table(ui, app);
-
-                if ui.button("⊞ Add soundfonts").clicked() {
-                    if let Some(mut paths) = FileDialog::new()
-                        .add_filter("Soundfonts", &["sf2"])
-                        .pick_files()
-                    {
-                        app.soundfonts.append(&mut paths);
-                    }
-                }
-            });
+            soundfont_table(ui, app);
         });
 
     egui::TopBottomPanel::bottom("playback_panel").show(ctx, |ui| {
@@ -38,78 +27,104 @@ pub(crate) fn draw_gui(ctx: &egui::Context, app: &mut SfontPlayer) {
 
     egui::CentralPanel::default().show(ctx, |ui| {
         song_table(ui, app);
-        if ui.button("⊞ Add songs").clicked() {
-            if let Some(mut paths) = FileDialog::new()
-                .add_filter("Midi files", &["mid"])
-                .pick_files()
-            {
-                app.midis.append(&mut paths);
-            }
-        }
     });
 }
 
 fn soundfont_table(ui: &mut egui::Ui, app: &mut SfontPlayer) {
-    TableBuilder::new(ui)
-        .striped(true)
-        .column(Column::exact(32.))
-        .column(Column::remainder())
-        .header(20.0, |mut header| {
-            header.col(|_| {});
-            header.col(|ui| {
-                ui.heading("Soundfont");
-            });
-        })
-        .body(|mut body| {
-            for (i, sf) in app.soundfonts.clone().iter().enumerate() {
-                body.row(TBL_ROW_H, |mut row| {
-                    row.col(|ui| {
-                        if ui.button("❎").clicked() {
-                            app.remove_sf(i)
-                        }
-                    });
-                    row.col(|ui| {
-                        let name = sf.file_name().unwrap().to_str().unwrap().to_owned();
-                        let highlight = Some(i) == app.selected_sf;
-                        if ui.add(egui::Button::new(name).frame(highlight)).clicked() {
-                            app.selected_sf = Some(i);
-                        }
-                    });
+    egui::ScrollArea::vertical().show(ui, |ui| {
+        TableBuilder::new(ui)
+            .striped(true)
+            .vscroll(false)
+            .column(Column::exact(32.))
+            .column(Column::remainder())
+            .header(20.0, |mut header| {
+                header.col(|_| {});
+                header.col(|ui| {
+                    ui.heading("Soundfont");
                 });
+            })
+            .body(|mut body| {
+                for (i, sf) in app.soundfonts.clone().iter().enumerate() {
+                    body.row(TBL_ROW_H, |mut row| {
+                        row.col(|ui| {
+                            if ui.button("❎").clicked() {
+                                app.remove_sf(i)
+                            }
+                        });
+                        row.col(|ui| {
+                            let name = sf.file_name().unwrap().to_str().unwrap().to_owned();
+                            let highlight = Some(i) == app.selected_sf;
+                            if ui.add(egui::Button::new(name).frame(highlight)).clicked() {
+                                app.selected_sf = Some(i);
+                            }
+                        });
+                    });
+                }
+            });
+        ui.horizontal(|ui| {
+            if ui.button("⊞ Add soundfonts").clicked() {
+                if let Some(mut paths) = FileDialog::new()
+                    .add_filter("Soundfonts", &["sf2"])
+                    .pick_files()
+                {
+                    app.soundfonts.append(&mut paths);
+                }
+            }
+            if ui.button("Clear").clicked() {
+                app.soundfonts.clear();
+                app.selected_sf = None;
             }
         });
+    });
 }
 
 fn song_table(ui: &mut egui::Ui, app: &mut SfontPlayer) {
-    TableBuilder::new(ui)
-        .striped(true)
-        .column(Column::exact(32.))
-        .column(Column::remainder())
-        .header(20.0, |mut header| {
-            header.col(|_| {});
-            header.col(|ui| {
-                ui.heading("Midis");
-            });
-        })
-        .body(|mut body| {
-            for (i, mid) in app.midis.clone().iter().enumerate() {
-                body.row(TBL_ROW_H, |mut row| {
-                    row.col(|ui| {
-                        if ui.button("❎").clicked() {
-                            app.remove_midi(i)
-                        }
-                    });
-                    row.col(|ui| {
-                        let name = mid.file_name().unwrap().to_str().unwrap().to_owned();
-                        let highlight = Some(i) == app.selected_midi;
-                        if ui.add(egui::Button::new(name).frame(highlight)).clicked() {
-                            app.selected_midi = Some(i);
-                            app.start();
-                        }
-                    });
+    egui::ScrollArea::vertical().show(ui, |ui| {
+        TableBuilder::new(ui)
+            .striped(true)
+            .vscroll(false)
+            .column(Column::exact(32.))
+            .column(Column::remainder())
+            .header(20.0, |mut header| {
+                header.col(|_| {});
+                header.col(|ui| {
+                    ui.heading("Midis");
                 });
+            })
+            .body(|mut body| {
+                for (i, mid) in app.midis.clone().iter().enumerate() {
+                    body.row(TBL_ROW_H, |mut row| {
+                        row.col(|ui| {
+                            if ui.button("❎").clicked() {
+                                app.remove_midi(i)
+                            }
+                        });
+                        row.col(|ui| {
+                            let name = mid.file_name().unwrap().to_str().unwrap().to_owned();
+                            let highlight = Some(i) == app.selected_midi;
+                            if ui.add(egui::Button::new(name).frame(highlight)).clicked() {
+                                app.selected_midi = Some(i);
+                                app.start();
+                            }
+                        });
+                    });
+                }
+            });
+        ui.horizontal(|ui| {
+            if ui.button("⊞ Add songs").clicked() {
+                if let Some(mut paths) = FileDialog::new()
+                    .add_filter("Midi files", &["mid"])
+                    .pick_files()
+                {
+                    app.midis.append(&mut paths);
+                }
+            }
+            if ui.button("Clear").clicked() {
+                app.midis.clear();
+                app.selected_midi = None;
             }
         });
+    });
 }
 
 fn playback_panel(ui: &mut egui::Ui, app: &mut SfontPlayer) {
@@ -123,12 +138,18 @@ fn playback_panel(ui: &mut egui::Ui, app: &mut SfontPlayer) {
                 app.pause();
             }
         } else {
-            if ui.add_enabled(app.can_play(), egui::Button::new("▶")).clicked() {
+            if ui
+                .add_enabled(app.can_play(), egui::Button::new("▶"))
+                .clicked()
+            {
                 app.play()
             }
         }
         // Stop button
-        if ui.add_enabled(app.is_playing(), egui::Button::new("⏹")).clicked() {
+        if ui
+            .add_enabled(app.is_playing(), egui::Button::new("⏹"))
+            .clicked()
+        {
             app.stop()
         }
         // Slider
@@ -136,10 +157,13 @@ fn playback_panel(ui: &mut egui::Ui, app: &mut SfontPlayer) {
         let mut pos = app.get_midi_position();
         ui.horizontal(|ui| {
             ui.spacing_mut().slider_width = 300.0;
-            ui.add(egui::Slider::new(&mut pos, 0.0..=len).show_value(false).trailing_fill(true));
+            ui.add(
+                egui::Slider::new(&mut pos, 0.0..=len)
+                    .show_value(false)
+                    .trailing_fill(true),
+            );
         });
-        
-        ui.label(format!("{:.0}/{:.0}", pos, len));
 
+        ui.label(format!("{:.0}/{:.0}", pos, len));
     });
 }
