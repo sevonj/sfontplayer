90a978c776e76a1c06763280932c42aca8ff3dc1
sevonj <100710152+sevonj@users.noreply.github.com>
2024-12-12 13:59:25 +0200

    Refactor: Font/song idx/sort is stored in app memory

--- src/player/playlist/serialize_playlist.rs
+++ src/player/playlist/serialize_playlist.rs
@@ -3,13 +3,7 @@
 
 use std::{convert::Into, fs::File, io::Write, path::PathBuf};
 
-use super::{
-    enums::{FileListMode, SongSort},
-    font_meta::FontMeta,
-    midi_meta::MidiMeta,
-    Playlist,
-};
-use crate::player::soundfont_list::FontSort;
+use super::{enums::FileListMode, font_meta::FontMeta, midi_meta::MidiMeta, Playlist};
 use anyhow::bail;
 use relative_path::{PathExt, RelativePath};
 use serde_json::{json, Value};
@@ -23,16 +17,12 @@ impl From<&Playlist> for Value {
                 json! ({"name": playlist.name,
 
                      "fonts": playlist.fonts,
-                     "font_idx": playlist.font_idx,
                      "font_list_mode": playlist.font_list_mode as u8,
                      "font_dir": playlist.font_dir,
-                     "font_sort": playlist.font_sort as u8,
 
                      "songs": playlist.midis,
-                     "song_idx": playlist.midi_idx,
                      "song_list_mode": playlist.song_list_mode as u8,
                      "song_dir": playlist.midi_dir,
-                     "song_sort": playlist.song_sort as u8
                     }
                 )
             },
@@ -66,16 +56,12 @@ impl From<&Playlist> for Value {
                      "name": playlist.name,
 
                      "fonts": fonts,
-                     "font_idx": playlist.font_idx,
                      "font_list_mode": playlist.font_list_mode as u8,
                      "font_dir": font_dir,
-                     "font_sort": playlist.font_sort as u8,
 
                      "songs": songs,
-                     "song_idx": playlist.midi_idx,
                      "song_list_mode": playlist.song_list_mode as u8,
                      "song_dir": song_dir,
-                     "song_sort": playlist.song_sort as u8
                     }
                 )
             },
@@ -90,38 +76,26 @@ impl From<Value> for Playlist {
             name: value["name"].as_str().unwrap_or("Name Missing!").into(),
 
             fonts: vec![],
-            font_idx: value["font_idx"].as_u64().map(|idx| idx as usize),
             font_list_mode: value["font_list_mode"]
                 .as_u64()
                 .map_or_else(FileListMode::default, |int| {
                     FileListMode::try_from(int as u8).unwrap_or_default()
                 }),
             font_dir: value["font_dir"].as_str().map(Into::into),
-            font_sort: value["font_sort"]
-                .as_u64()
-                .map_or_else(FontSort::default, |int| {
-                    FontSort::try_from(int as u8).unwrap_or_default()
-                }),
 
             midis: vec![],
-            midi_idx: value["song_idx"].as_u64().map(|idx| idx as usize),
             song_list_mode: value["song_list_mode"]
                 .as_u64()
                 .map_or_else(FileListMode::default, |int| {
                     FileListMode::try_from(int as u8).unwrap_or_default()
                 }),
             midi_dir: value["song_dir"].as_str().map(Into::into),
-            song_sort: value["song_sort"]
-                .as_u64()
-                .map_or_else(SongSort::default, |int| {
-                    SongSort::try_from(int as u8).unwrap_or_default()
-                }),
 
             ..Default::default()
         };
 
         if let Some(fonts_json) = value["fonts"].as_array() {
-            for (i, data) in fonts_json.iter().enumerate() {
+            for data in fonts_json {
                 let fontmeta = if let Ok(meta) = FontMeta::try_from(data) {
                     meta
                 } else {
@@ -130,12 +104,6 @@ impl From<Value> for Playlist {
                         .as_str()
                         .map(|path| FontMeta::new(path.into()))
                     else {
-                        // Give up
-                        match playlist.font_idx {
-                            Some(idx) if idx > i => playlist.font_idx = Some(idx - 1),
-                            Some(idx) if idx == i => playlist.font_idx = None,
-                            _ => (),
-                        }
                         continue;
                     };
                     meta
@@ -145,7 +113,7 @@ impl From<Value> for Playlist {
         }
 
         if let Some(songs_json) = value["songs"].as_array() {
-            for (i, data) in songs_json.iter().enumerate() {
+            for data in songs_json {
                 let midimeta = if let Ok(meta) = MidiMeta::try_from(data) {
                     meta
                 } else {
@@ -154,28 +122,12 @@ impl From<Value> for Playlist {
                         .as_str()
                         .map(|path| MidiMeta::new(path.into()))
                     else {
-                        // Give up
-                        match playlist.font_idx {
-                            Some(idx) if idx > i => playlist.midi_idx = Some(idx - 1),
-                            Some(idx) if idx == i => playlist.midi_idx = None,
-                            _ => (),
-                        }
                         continue;
                     };
                     meta
                 };
                 playlist.midis.push(midimeta);
             }
-        }
-
-        let font_out_of_bounds = playlist.font_idx.is_some_and(|i| i >= playlist.fonts.len());
-        let song_out_of_bounds = playlist.midi_idx.is_some_and(|i| i >= playlist.midis.len());
-
-        if playlist.fonts.is_empty() || font_out_of_bounds {
-            playlist.font_idx = None;
-        }
-        if playlist.midis.is_empty() || song_out_of_bounds {
-            playlist.midi_idx = None;
         }
 
         playlist
@@ -308,106 +260,6 @@ mod tests {
         assert_eq!(new_playlist_non.midi_dir, None);
         let dir_path = new_playlist_dir.midi_dir.unwrap();
         assert_eq!(dir_path.to_str().unwrap(), "Fakepath");
-    }
-
-    #[test]
-    fn test_fontsort() {
-        let mut playlist_name_asc = Playlist::default();
-        let mut playlist_name_desc = Playlist::default();
-        let mut playlist_size_asc = Playlist::default();
-        let mut playlist_size_desc = Playlist::default();
-        playlist_name_asc.font_sort = FontSort::NameAsc;
-        playlist_name_desc.font_sort = FontSort::NameDesc;
-        playlist_size_asc.font_sort = FontSort::SizeAsc;
-        playlist_size_desc.font_sort = FontSort::SizeDesc;
-        let new_playlist_name_asc = run_serialize(playlist_name_asc);
-        let new_playlist_name_desc = run_serialize(playlist_name_desc);
-        let new_playlist_size_asc = run_serialize(playlist_size_asc);
-        let new_playlist_size_desc = run_serialize(playlist_size_desc);
-        assert_eq!(new_playlist_name_asc.font_sort, FontSort::NameAsc);
-        assert_eq!(new_playlist_name_desc.font_sort, FontSort::NameDesc);
-        assert_eq!(new_playlist_size_asc.font_sort, FontSort::SizeAsc);
-        assert_eq!(new_playlist_size_desc.font_sort, FontSort::SizeDesc);
-    }
-
-    #[test]
-    fn test_songsort() {
-        let mut playlist_name_asc = Playlist::default();
-        let mut playlist_name_desc = Playlist::default();
-        let mut playlist_time_asc = Playlist::default();
-        let mut playlist_time_desc = Playlist::default();
-        let mut playlist_size_asc = Playlist::default();
-        let mut playlist_size_desc = Playlist::default();
-        playlist_name_asc.song_sort = SongSort::NameAsc;
-        playlist_name_desc.song_sort = SongSort::NameDesc;
-        playlist_time_asc.song_sort = SongSort::TimeAsc;
-        playlist_time_desc.song_sort = SongSort::TimeDesc;
-        playlist_size_asc.song_sort = SongSort::SizeAsc;
-        playlist_size_desc.song_sort = SongSort::SizeDesc;
-        let new_playlist_name_asc = run_serialize(playlist_name_asc);
-        let new_playlist_name_desc = run_serialize(playlist_name_desc);
-        let new_playlist_time_asc = run_serialize(playlist_time_asc);
-        let new_playlist_time_desc = run_serialize(playlist_time_desc);
-        let new_playlist_size_asc = run_serialize(playlist_size_asc);
-        let new_playlist_size_desc = run_serialize(playlist_size_desc);
-        assert_eq!(new_playlist_name_asc.song_sort, SongSort::NameAsc);
-        assert_eq!(new_playlist_name_desc.song_sort, SongSort::NameDesc);
-        assert_eq!(new_playlist_time_asc.song_sort, SongSort::TimeAsc);
-        assert_eq!(new_playlist_time_desc.song_sort, SongSort::TimeDesc);
-        assert_eq!(new_playlist_size_asc.song_sort, SongSort::SizeAsc);
-        assert_eq!(new_playlist_size_desc.song_sort, SongSort::SizeDesc);
-    }
-
-    #[test]
-    fn test_fontidx_valid_is_unchanged() {
-        let mut playlist_69 = Playlist::default();
-        let mut playlist_none = Playlist::default();
-        for i in 0..=70 {
-            playlist_69.add_font(format!("Fakepath{i}").into()).unwrap();
-        }
-        playlist_69.font_idx = Some(69);
-        playlist_none.font_idx = None;
-        let new_playlist_69 = run_serialize(playlist_69);
-        let new_playlist_none = run_serialize(playlist_none);
-        assert_eq!(new_playlist_69.font_idx, Some(69));
-        assert_eq!(new_playlist_none.font_idx, None);
-    }
-
-    #[test]
-    fn test_songidx_valid_is_unchanged() {
-        let mut playlist_69 = Playlist::default();
-        let mut playlist_none = Playlist::default();
-        for i in 0..=70 {
-            playlist_69.add_song(format!("Fakepath{i}").into()).unwrap();
-        }
-        playlist_69.midi_idx = Some(69);
-        playlist_none.midi_idx = None;
-        let new_playlist_69 = run_serialize(playlist_69);
-        let new_playlist_none = run_serialize(playlist_none);
-        assert_eq!(new_playlist_69.midi_idx, Some(69));
-        assert_eq!(new_playlist_none.midi_idx, None);
-    }
-
-    #[test]
-    fn test_fontidx_outofbounds_becomes_none() {
-        let mut playlist_69 = Playlist::default();
-        for i in 0..=7 {
-            playlist_69.add_font(format!("Fakepath{i}").into()).unwrap();
-        }
-        playlist_69.font_idx = Some(69);
-        let new_playlist_69 = run_serialize(playlist_69);
-        assert_eq!(new_playlist_69.font_idx, None);
-    }
-
-    #[test]
-    fn test_songidx_outofbounds_becomes_none() {
-        let mut playlist_69 = Playlist::default();
-        for i in 0..=7 {
-            playlist_69.add_song(format!("Fakepath{i}").into()).unwrap();
-        }
-        playlist_69.midi_idx = Some(69);
-        let new_playlist_69 = run_serialize(playlist_69);
-        assert_eq!(new_playlist_69.midi_idx, None);
     }
 
     #[test]
